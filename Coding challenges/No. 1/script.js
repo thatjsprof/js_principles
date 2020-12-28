3b40bd218bdc1ca5b239dac8e9a0796ff82945b0
David Ajayi <davidrums65@gmail.com>
2020-12-28 12:40:36 +0100

    completed fisrt coding challenge 1a

---
+++ Coding challenges/No. 1/script.js
@@ -0,0 +1,5 @@
+var johnH = 20, johnM = 10, markH = 30, markM = 15
+var johnBMI = johnM / johnH ^ 2
+var markBMI = markM / markH ^ 2
+var isHigher = markBMI > johnBMI
+console.log('Is Mark\'s BMI higher than John\'s?', isHigher)
