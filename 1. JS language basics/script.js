e42ef988c09b3ac48de532ae868d1e1a637f9dfa
David Ajayi <davidrums65@gmail.com>
2021-01-01 21:47:15 +0100

    learnt about the this keyword

--- 1. JS language basics/script.js
+++ 1. JS language basics/script.js
@@ -121,8 +121,12 @@ E.g
         - The argument object is created which contains all the arguments that were passed into the function
         - The code is scanned for function declarations for each function a property is created in the variable object pointing to the function
         - The code is scanned also for variable declarations. This principle is referred to as hoisting which means that the variables and funcrions are declared before the code is run. variables are set to undefined
-    - Scoping and scope chain
+    - Scoping and scope chain:
         - Scoping answers the question where can we access a variable
         - Each new function creates a scope, the space in which the variables it defines are accessible
         - Lexical scoping: a function that is within another function gets access to the scope of thw outer function
+    - The this keyword:
+        - Regular function call: the this keyword points to the global object
+        - Method call: the this variable points to the object that is calling the method
+        - The this variable is not assigned a value until the function where it is called is defined
 */
