54ac123e01277cc5eb00a17b6c44533350de7ef9
David Ajayi <davidrums65@gmail.com>
2021-01-03 14:53:56 +0100

    organized folder structure '

---
+++ 3. JS DOM and DOM manipulation/script.js
@@ -0,0 +1,8 @@
+// The DOM and DOM manipulation
+/*
+- means document object model
+- connects html with scripted languages
+- for each html box there is a object in the DOM that we can use to access and interact with
+- Events: notifications we make to code
+- Event listener: a function that performs an action based on the occurence of a particular event
+*/
