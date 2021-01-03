99f1f81b5c583b7105d3afdbdf89b236380b64bc
David Ajayi <davidrums65@gmail.com>
2021-01-03 16:51:50 +0100

    did a few examples using function constructor and object.create

--- 4. JS Advanced objects and functions/script.js
+++ 4. JS Advanced objects and functions/script.js
@@ -5,4 +5,33 @@
 - it is used to keep our code structured and organized
 - Learnt about prototypes and the prototype chain
 - Learnt how to create objects using function constructors
+- Learnt how to create objects using object.create
 */
+
+// Function constructor
+var Person = function(name, age) {
+    this.name = name
+    this.age = age
+}
+Person.prototype.calculateAge = function() {
+    return this.age
+}
+var john = new Person('john', 12)
+console.log(john.calculateAge())
+
+// Object.create
+var PersonProto = {
+    calculateAge: function(){
+        return this.age
+    }
+}
+var john1 = Object.create(PersonProto, {
+    name: { value: 'john' },
+    age: { value: 23 },
+    show: { value: function() {
+            return this
+        }
+    }
+})
+john1.name = 'mark'
+console.log(john1.show())
