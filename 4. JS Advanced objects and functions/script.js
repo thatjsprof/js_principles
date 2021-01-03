bbe470218f9d4e5daf9e58b0a37c26a6a221509c
David Ajayi <davidrums65@gmail.com>
2021-01-03 17:40:02 +0100

    learnt how to return functions from functions

--- 4. JS Advanced objects and functions/script.js
+++ 4. JS Advanced objects and functions/script.js
@@ -7,6 +7,8 @@
 - Learnt how to create objects using function constructors
 - Learnt how to create objects using object.create
 - learnt more about objects and primitives
+- Learnt how to pass functions as argumants
+- Learnt how to return functions from functions
 */
 
 // Function constructor
@@ -36,3 +38,35 @@ var john1 = Object.create(PersonProto, {
 })
 john1.name = 'mark'
 console.log(john1.show())
+
+// functions as arguments
+var arr = [12, 13, 14]
+
+function mutate(arr, calculate) {
+    let arrRes = []
+    for(var i = 0; i < arr.length; i++) {
+        arrRes.push(calculate(arr[i]))
+    }
+    return arrRes
+}
+
+function calculate(val) {
+    return val + 10
+}
+
+console.log(mutate(arr, calculate))
+
+// returning functions
+function calculate(occ) {
+    if(occ == 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach?', name)
+        }
+    }else{
+        return function(name) {
+            console.log('you are a', occ)
+        }
+    }
+}
+var occ = calculate('farmer')
+occ('mary')
