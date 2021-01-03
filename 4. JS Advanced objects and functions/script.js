35bf722b4971bc91b8dddb7540173eb06e4c6b1e
David Ajayi <davidrums65@gmail.com>
2021-01-03 16:27:39 +0100

    advanced javascript functions and objects

--- 4. JS Advanced objects and functions/script.js
+++ 4. JS Advanced objects and functions/script.js
@@ -1,3 +1,8 @@
 // Advanced JavaScript objects and functions (looked into js objects and functions in greater detail)
 /*
+- Everything is an object except primitives
+- object oriented programming involves objects interating with one another through methods and properties
+- it is used to keep our code structured and organized
+- Learnt about prototypes and the prototype chain
+- Learnt how to create objects using function constructors
 */
