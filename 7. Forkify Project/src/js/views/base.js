99c16a6c3dd9a6c9b51fe956ec6f384e49daf699
David Ajayi <davidrums65@gmail.com>
2021-01-13 10:08:12 +0100

    created renderLoader and clearLoader functions

--- 7. Forkify Project/src/js/views/base.js
+++ 7. Forkify Project/src/js/views/base.js
@@ -1,5 +1,28 @@
 export const elements = {
     searchInput: document.querySelector('.search__field'),
     searchForm: document.querySelector('.search'),
-    searchResList: document.querySelector('.results__list')
+    searchResList: document.querySelector('.results__list'),
+    searchRes: document.querySelector('.results')
+}
+
+export const elementStrings = {
+    loader: 'loader'
+}
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `
+    parent.insertAdjacentHTML('afterbegin', loader)
+}
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`)
+    if(loader) {
+        loader.parentElement.removeChild(loader)
+    }
 }
