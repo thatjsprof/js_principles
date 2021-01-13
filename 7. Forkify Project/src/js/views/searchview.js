c48f24be49cbec212ca7066f6ef5342915cadbf3
David Ajayi <davidrums65@gmail.com>
2021-01-13 10:10:19 +0100

    minor changes

--- 7. Forkify Project/src/js/views/searchview.js
+++ 7. Forkify Project/src/js/views/searchview.js
@@ -3,13 +3,13 @@ import {elements} from './base'
 const renderRecipe = (recipe) => {
     const markup = `
         <li>
-            <a class="results__link" href="#${recipe.recipe_id}">
+            <a class="results__link" href="#${recipe.id}">
                 <figure class="results__fig">
-                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                    <img src="${recipe.image}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
                     <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
-                    <p class="results__author">${recipe.publisher}</p>
+                    <p class="results__author">David</p>
                 </div>
             </a>
         </li>
@@ -19,10 +19,13 @@ const renderRecipe = (recipe) => {
 }
 
 const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = []
     if(title.length > limit) {
-        // 
-    }else {
-        // 
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit) newTitle.push(cur)
+            return acc + cur.length
+        }, 0)
+        return `${newTitle.join(' ')} ...`
     }
     return title
 }
