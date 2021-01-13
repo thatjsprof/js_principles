9ef12e844221c61b9e52b9975e44cf6649c3eaba
David Ajayi <davidrums65@gmail.com>
2021-01-13 09:19:03 +0100

    made some changes to webpack configuration

--- 7. Forkify Project/webpack.config.js
+++ 7. Forkify Project/webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './src/index.html'
+            template: './src/index.html',
+            inject: false
         })
     ],
     module: {
