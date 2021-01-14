3ad2ee3f4392b21b5a64320709bae11ce592eac8
David Ajayi <davidrums65@gmail.com>
2021-01-14 10:28:30 +0100

    added new property to elements object

--- 7. Forkify Project/src/js/views/base.js
+++ 7. Forkify Project/src/js/views/base.js
@@ -3,7 +3,8 @@ export const elements = {
     searchForm: document.querySelector('.search'),
     searchResList: document.querySelector('.results__list'),
     searchRes: document.querySelector('.results'),
-    searchResPages: document.querySelector('.results__pages')
+    searchResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe')
 }
 
 export const elementStrings = {
