022bef172a76634526c122f9c79d4b206ce50637
David Ajayi <davidrums65@gmail.com>
2021-01-14 09:25:31 +0100

    implemented parseIngredients function

--- 7. Forkify Project/src/js/models/recipe.js
+++ 7. Forkify Project/src/js/models/recipe.js
@@ -21,5 +21,61 @@ export default class Recipe {
 
     calcTime() {
         const numIng = this.ingredients.length
+        const periods = Math.ceil(numIng / 3)
+        this.time = periods * 15
+    }
+
+    calcServings() {
+        this.servings = 4
+    }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cup', '']
+        const unitShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp']
+
+        const newIngredients = this.ingredients.map(el => {
+            let ingredient = el.toLowerCase() // uniform units
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitShort[i])
+            })
+
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ') // remove parenthesis
+            
+            const arrIng = ingredient.split(' ')
+            const unitIndex = arrIng.findIndex(el2 => {
+                unitShort.includes(el2)
+            })
+
+            let obj
+            if(unitIndex > -1) {
+                const arrCount = arrIng().slice(0, unitIndex)
+                let count
+                if(arrCount.length === 1) {
+                    count = eval(arrIng[0].replace('-', '+'))
+                }else {
+                    count = eval(arrIng().slice(0, unitIndex).join('+'))
+                }
+                obj = {
+                    count: count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng().slice(unitIndex + 1).join(' ')
+                }
+            }else if(parseInt(arrIng[0], 10)) {
+                obj = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient = arrIng.slice(1).join(' ')
+                }
+            }else if(unitIndex === -1) {
+                obj = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                }
+            }
+
+            return obj
+        })
+        this.ingredients = newIngredients
     }
 }
