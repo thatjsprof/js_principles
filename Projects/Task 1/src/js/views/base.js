5701328a9fe90300762699c78bd203cfcd59b87b
David Ajayi <davidrums65@gmail.com>
2021-02-03 12:16:34 +0100

    some minor stuff

--- Projects/Task 1/src/js/views/base.js
+++ Projects/Task 1/src/js/views/base.js
@@ -3,5 +3,8 @@ export const elements = {
     todoList: document.querySelector('.activeTodo'),
     input: document.querySelector('#todo-input'),
     errorText: document.querySelector('.text-danger'),
-    inactiveTodo: document.querySelector('.inactiveTodo')
+    inactiveTodo: document.querySelector('.inactiveTodo'),
+    actTodo: document.querySelector('.act-todo'),
+    delTodo: document.querySelector('.del-todo'),
+    deleteAll: document.querySelector('.delete-all'),
 }
