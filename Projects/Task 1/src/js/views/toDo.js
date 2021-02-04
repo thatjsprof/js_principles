d38a296540eff476e8ba99d4d167944a700ac760
David Ajayi <davidrums65@gmail.com>
2021-02-04 10:50:48 +0100

    more views stuff

--- Projects/Task 1/src/js/views/toDo.js
+++ Projects/Task 1/src/js/views/toDo.js
@@ -30,9 +30,43 @@ export const renderToDo = (todo) => {
     elements.todoList.insertAdjacentHTML('beforeend', markup)
 }
 
-export const clear = () => {
+export const clearTodo = () => {
     elements.todoList.innerHTML = ''
+}
+
+export const clearInactive = () => {
     elements.inactiveTodo.innerHTML = ''
+}
+
+export const clearButtons = () => {
+    elements.buttons.innerHTML = ''
+}
+
+const createButton = (page, type) => {
+    return `
+        <button class="btn btn-outline-info todos__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+            <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+            <span class="fa fa-arrow-${type === 'prev' ? 'left' : 'right'}"></span>
+        </button>
+    `
+}
+
+const renderButtons = (page, deletedTodos, result_per_page) => {
+    const pages = Math.ceil(deletedTodos / result_per_page)
+
+    let button
+    if (page === 1 && pages > 1) {
+        button = createButton(page, 'next')
+    } else if (page < pages) {
+        button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}
+        `
+    } else if (page === pages && page > 1) {
+        button = createButton(page, 'prev')
+    }
+
+    if (button) elements.buttons.insertAdjacentHTML('afterbegin', `<br>${button}`)
 }
 
 export const showRecentlyDeleted = (removedToDo) => {
@@ -43,4 +77,11 @@ export const showRecentlyDeleted = (removedToDo) => {
         </li>
     `
     elements.inactiveTodo.insertAdjacentHTML('beforeend', markup)
+}
+
+export const renderDeletedTodo = (deletedTodos, page = 1, result_per_page = 5) => {
+    const start = (page - 1) * result_per_page
+    const end = page * result_per_page
+    deletedTodos.slice(start, end).forEach(showRecentlyDeleted)
+    renderButtons(page, deletedTodos.length, result_per_page)
 }
