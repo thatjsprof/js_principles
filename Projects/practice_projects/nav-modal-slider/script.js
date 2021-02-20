ee0b978a7d7c477f0915a8c9a3a1c33f6d317938
David Ajayi <davidrums65@gmail.com>
2021-02-20 21:57:26 +0100

    changed files

--- Projects/practice_projects/nav-modal-slider/script.js
+++ Projects/practice_projects/nav-modal-slider/script.js
@@ -1,5 +1,15 @@
 const navButton = document.querySelector('.header__nav').addEventListener('click', showNavigation)
 
+const modal = document.querySelector('.modal')
+
+const button = document.querySelector('.btn--modal').addEventListener('click', () => {
+    modal.classList.add('show-modal')
+})
+
 function showNavigation(e) {
     document.body.classList.toggle('sidenav')
 }
+
+window.addEventListener('click', (e) => {
+    e.target === modal ? modal.classList.remove('show-modal') : ''
+})
